16d9629b7ce09f8cd043230d1f343659eefddb9b
Nick <>
2022-06-26 19:06:41 +0100

    fix a few things

--- run_get_data.sh
+++ run_get_data.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-#/usr/local/python-3.8.9/bin/bin/python3 /home/nbooth/n3rgy/get-data.py
-#/usr/local/python-3.8.9/bin/bin/python3 /home/nbooth/n3rgy/get-data-gas.py
-/usr/local/python-3.8.9/bin/bin/python3 /home/nbooth/n3rgy/get_n3rgy_data.py electricity power2
-/usr/local/python-3.8.9/bin/bin/python3 /home/nbooth/n3rgy/get_n3rgy_data.py gas power2
+/usr/local/python-3.8.9/bin/bin/python3 /usr/local/n3rgy/get_n3rgy_data.py electricity power2
+/usr/local/python-3.8.9/bin/bin/python3 /usr/local/n3rgy/get_n3rgy_data.py gas power2
