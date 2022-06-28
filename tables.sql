f15a90dbd3273653b7109fece3610b8152b89be3
Nick <>
2022-06-28 18:53:24 +0100

    update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,14 +1,15 @@
 
 create table smartmeter_consumption(
-energy_this_period float NOT NULL,
+electricity float default NULL,
+gas float default NULL
 timestamp DATETIME NOT NULL, 
 UNIQUE (timestamp)
 );
 
 
-alter table smartmeter_consumption add column gas float;
-alter table smartmeter_consumption modify energy_this_period float default NULL;
-alter table smartmeter_consumption change energy_this_period electricity float;
+#alter table smartmeter_consumption add column gas float;
+#alter table smartmeter_consumption modify energy_this_period float default NULL;
+#alter table smartmeter_consumption change energy_this_period electricity float;
 create unique index index_smartmeter_consumption on smartmeter_consumption (timestamp);
 
 create table smartmeter_last_timestamps(
