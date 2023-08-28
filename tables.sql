bd8d217507a0cac4dd8b632a45537c4188d48f95
Nick <21123527+nthbooth@users.noreply.github.comcd>
2023-08-28 21:12:03 +0100

    table rename start and solaredge stuff added

--- tables.sql
+++ tables.sql
@@ -4,7 +4,18 @@ electricity double default NULL,
 electricity_production double default NULL,
 gas_kwh double default NULL,
 gas double default NULL,
-timestamp DATETIME NOT NULL, 
+timestamp DATETIME NOT NULL PRIMARY KEY, 
+UNIQUE (timestamp)
+);
+
+
+create table power_data(
+electricity double default NULL,
+electricity_production double default NULL,
+gas_kwh double default NULL,
+gas double default NULL,
+solar_generation double default NULL,
+timestamp DATETIME NOT NULL PRIMARY KEY,
 UNIQUE (timestamp)
 );
 
@@ -12,6 +23,19 @@ UNIQUE (timestamp)
 create unique index index_smartmeter_consumption on smartmeter_consumption (timestamp);
 
 create table smartmeter_last_timestamps(
+metric varchar(30) not null PRIMARY KEY,
+timestamp DATETIME not null
+);
+
+create table solaredge(
+electricity_production double default NULL,
+timestamp DATETIME NOT NULL PRIMARY KEY, 
+UNIQUE (timestamp)
+);
+
+create unique index index_solaredge on solaredge (timestamp);
+
+create table solaredge_last_timestamps(
 metric varchar(30) not null PRIMARY KEY,
 timestamp DATETIME not null
 );
